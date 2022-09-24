3ca089d469079980e8383327f4a10d1e60c4023e
kenny <83075594+ktaejung01@users.noreply.github.com>
2022-09-24 11:09:43 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -153,13 +153,13 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.BOLT_EUV:
       ret.minEnableSpeed = -1
-      ret.mass = 1669. + STD_CARGO_KG
-      ret.wheelbase = 2.63779
-      ret.steerRatio = 18.09
-      ret.centerToFront = 2.34 # measured
+      ret.mass = 1600. + STD_CARGO_KG
+      ret.wheelbase = 2.78
+      ret.steerRatio = 18.1
+      ret.centerToFront = 2.17 # measured
       ##ret.centerToFront = ret.wheelbase * 0.5
-      tire_stiffness_factor = 0.444
-      ret.steerActuatorDelay = 0.3
+      tire_stiffness_factor = 1.0
+      ret.steerActuatorDelay = 0.25
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
     elif candidate == CAR.SILVERADO:
