eb76550957b62fbdac758b856b7cb8aeff24c6fb
kenny <83075594+ktaejung01@users.noreply.github.com>
2022-10-13 14:50:12 +0900

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -43,16 +43,15 @@ class CarController:
     can_sends = []
 
     # Steering (50Hz)
+    # Initialize ASCMLKASteeringCmd counter using the camera
+    if self.frame == 0 and self.CP.networkLocation == NetworkLocation.fwdCamera:
+      self.lka_steering_cmd_counter = CS.camera_lka_steering_cmd_counter
+
     # Avoid GM EPS faults when transmitting messages too close together: skip this transmit if we just received the
     # next Panda loopback confirmation in the current CS frame.
     if CS.loopback_lka_steering_cmd_updated:
       self.lka_steering_cmd_counter += 1
-      self.sent_lka_steering_cmd = True
     elif (self.frame % self.params.STEER_STEP) == 0:
-      # Initialize ASCMLKASteeringCmd counter using the camera
-      if not self.sent_lka_steering_cmd and self.CP.networkLocation == NetworkLocation.fwdCamera:
-        self.lka_steering_cmd_counter = CS.camera_lka_steering_cmd_counter + 1
-
       if CC.latActive:
         new_steer = int(round(actuators.steer * self.params.STEER_MAX))
         apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.params)
@@ -62,7 +61,7 @@ class CarController:
       self.apply_steer_last = apply_steer
       idx = self.lka_steering_cmd_counter % 4
       can_sends.append(gmcan.create_steering_control(self.packer_pt, CanBus.POWERTRAIN, apply_steer, idx, CC.latActive))
-
+   
     if self.CP.openpilotLongitudinalControl:
       # Gas/regen, brakes, and UI commands - all at 25Hz
       if self.frame % 4 == 0:
